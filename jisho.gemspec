a4f90072420ec054488c23233280c8d13922438d
Minh Phan <wofi.minh@1pac.vn>
2017-05-30 18:11:45 +0700

    Implement jisho

--- jisho.gemspec
+++ jisho.gemspec
@@ -9,9 +9,8 @@ Gem::Specification.new do |spec|
   spec.authors       = ["Minh Phan"]
   spec.email         = ["wofi.minh@1pac.vn"]
 
-  spec.summary       = %q{TODO: Write a short summary, because Rubygems requires one.}
-  spec.description   = %q{TODO: Write a longer description or delete this line.}
-  spec.homepage      = "TODO: Put your gem's website or public repo URL here."
+  spec.summary       = %q{An english-japanese dictionary using jisho.org API}
+  spec.homepage      = "https://github.com/wofi-minh/jisho"
   spec.license       = "MIT"
 
   # Prevent pushing this gem to RubyGems.org. To allow pushes either set the 'allowed_push_host'
@@ -19,13 +18,10 @@ Gem::Specification.new do |spec|
   if spec.respond_to?(:metadata)
     spec.metadata['allowed_push_host'] = "TODO: Set to 'http://mygemserver.com'"
   else
-    raise "RubyGems 2.0 or newer is required to protect against " \
-      "public gem pushes."
+    raise "RubyGems 2.0 or newer is required to protect against public gem pushes. "
   end
 
-  spec.files         = `git ls-files -z`.split("\x0").reject do |f|
-    f.match(%r{^(test|spec|features)/})
-  end
+  spec.files         = `git ls-files -z`.split("\x0").reject { |f| f.match(%r{^(test|spec|features)/}) }
   spec.bindir        = "exe"
   spec.executables   = spec.files.grep(%r{^exe/}) { |f| File.basename(f) }
   spec.require_paths = ["lib"]
--- lib/jisho.rb
+++ lib/jisho.rb
@@ -1,5 +1,75 @@
 require "jisho/version"
+require "net/http"
+require "json"
 
 module Jisho
-  # Your code goes here...
+  extend self
+
+  JISHO_API_URL = "http://jisho.org/api/v1"
+  CONSOLE_WIDTH = IO.console.winsize[1]
+  HEADERS = %w(kanji hiragana english)
+
+  def search(words)
+    path = "/search/words"
+    uri = URI(JISHO_API_URL)
+    uri.path << path
+    uri.query = URI.encode_www_form(keyword: words)
+    response = Net::HTTP.get_response(uri)
+    results(words, response.body)
+  end
+
+  def results(words, body)
+    print table_header(words)
+    print table_body(body)
+  end
+
+  def table_header(words)
+    output = "#{words}\n#{seperator}\n"
+    HEADERS.each_with_index do |header, index|
+      output += header.capitalize
+      break if index == 2
+      output += "#{whitespace}| "
+    end
+    output += "\n#{seperator}"
+  end
+
+  def seperator
+    output = ""
+    CONSOLE_WIDTH.times.each { |i| output += "-" }
+    output
+  end
+
+  def whitespace
+    output = ""
+    (CONSOLE_WIDTH / 3).times.each { |i| output += " " }
+    output
+  end
+
+  def table_body(results)
+    output = ""
+    results = format_results(results)
+    results.each do |r|
+      output += "\n"
+      HEADERS.each_with_index do |header, i|
+        output += r[header.to_sym]
+        break if i == 2
+        output += "#{whitespace}| "
+      end
+    end
+    output
+  end
+
+  def format_results(body)
+    formatted = []
+    parsed_body = JSON.parse(body)['data']
+    results = parsed_body.select { |m| m['is_common'] }.take(10)
+    results.each do |r|
+      formatted.push({
+        kanji: r["japanese"].first["word"],
+        hiragana: r["japanese"].first["reading"],
+        english: r["senses"].first["english_definitions"].join(', ')
+      })
+    end
+    formatted
+  end
 end
